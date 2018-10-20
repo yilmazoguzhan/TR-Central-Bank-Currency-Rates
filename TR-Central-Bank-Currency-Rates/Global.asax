376c9b2ef4b023139d3d2bf51dc10306e202d89a
Oğuzhan Yılmaz <oguzhan@dorabilisim.com>
2018-10-20 14:37:16 +0300

    Base files added.

---
+++ TR-Central-Bank-Currency-Rates/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TR_Central_Bank_Currency_Rates.MvcApplication" Language="C#" %>
---
+++ TR-Central-Bank-Currency-Rates/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TR_Central_Bank_Currency_Rates
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
