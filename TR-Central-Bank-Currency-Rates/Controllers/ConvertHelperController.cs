501668cca087244a757a7e6a9c00fb8e5c8a33b1
Oğuzhan Yılmaz <oguzhaanyilmaz@gmail.com>
2018-10-28 20:38:41 +0300

    Added git ignore and GetTodayXml method.

--- TR-Central-Bank-Currency-Rates/Controllers/ConvertHelperController.cs
+++ TR-Central-Bank-Currency-Rates/Controllers/ConvertHelperController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml;
 
 namespace TR_Central_Bank_Currency_Rates.Controllers
 {
@@ -11,11 +12,22 @@ namespace TR_Central_Bank_Currency_Rates.Controllers
     {
         //public class 
         LogController logController = new LogController();
+
         [HttpGet]
-        public IHttpActionResult GetToday()
+        public IHttpActionResult GetTodayXml()
         {
-            logController.WriteLog("Metoda girildi.");
-            return Json(new { test = "test"});
+            try
+            {
+                string todayXmlUrl = "http://www.tcmb.gov.tr/kurlar/today.xml";
+                var xmlDoc = new XmlDocument();
+                xmlDoc.Load(todayXmlUrl);
+                return Json(new { state = true, content = xmlDoc.InnerXml });
+            }
+            catch (Exception ex)
+            {
+                logController.WriteLog(ex.Message);
+                return Json(new { state = false, content = "Merkez Bankası xml bilgisi alınırken hata meydana geldi!" });
+            }
         }
     }
 }
